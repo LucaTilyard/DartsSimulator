fixed targeting off-by-one bug

main.py
@@ -2,7 +2,7 @@ from player import *
 import matplotlib.pyplot as plt
 from src.methods import *
 
-luke_the_fluke_shittler = Player(4)
+luke_the_fluke_shittler = Player(21)
 
 angles = np.linspace(1/20*np.pi, 2*np.pi+1/20*np.pi, 21)
 
@@ -32,6 +32,7 @@ plt.ylim(-1.345, 1.345)
 i = 0
 score = 0
 while i < 3:
+    luke_the_fluke_shittler.Aim(18, "Single")
     throw = luke_the_fluke_shittler.Throw(1)
     polar = ConvertToPolar(throw[0], throw[1])
     dartScore = CalculateValue(polar[0], polar[1])