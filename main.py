c9fee7d354784956374baf66df920378b57758dd
lachlanlatto <lachlanlatto@gmail.com>
2025-03-18 15:27:59 +0000

    cant remember now

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 from src.methods import *
 from Dartboardplot import plot_dartboard
 
-luke_the_fluke_shittler = Player(17)
+luke_the_fluke_shittler = Player(25)
 
 plot_dartboard()
 
 i = 0
-score = 501
-while i < 3:
+score = 1000000
+while i < 1000:
     if score <= 170 and score!= 169 and score != 168 and score != 166 and score != 165 and score != 163 and score != 162 and score != 159:
         checkout = luke_the_fluke_shittler.Checkout(score)
         print(checkout)
@@ -17,7 +17,7 @@ while i < 3:
         throw = luke_the_fluke_shittler.Throw()
         polar = ConvertToPolar(throw[0], throw[1])
         dartScore = CalculateValue(polar[0], polar[1])
-        plt.scatter(polar[1], polar[0], s=3, c='blue', marker='o')
+        plt.scatter(polar[1], polar[0], s=3, c='aqua', marker='o',zorder=2)
 
     else:
         luke_the_fluke_shittler.Aim(20 , "Treble")
@@ -25,7 +25,7 @@ while i < 3:
         throw = luke_the_fluke_shittler.Throw()
         polar = ConvertToPolar(throw[0], throw[1])
         dartScore = CalculateValue(polar[0], polar[1])
-        plt.scatter(polar[1], polar[0], s=3, c='aqua', marker='o')
+        plt.scatter(polar[1], polar[0], s=3, c='aqua', marker='o',zorder=2)
 
     score -= dartScore
     if score == 0:
