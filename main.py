26422c0478715f9474f3f55c2ed7d0d4a154766e
Luca Tilyard <Luca@Tilyard.co.uk>
2025-03-18 20:40:32 +0000

    refactored Dartboardplot.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-from player import *
+from src.player import *
 import matplotlib.pyplot as plt
 from src.methods import *
-from Dartboardplot import plot_dartboard
+from src.Dartboardplot import plot_dartboard
 
-luke_the_fluke_shittler = Player(25)
+luke_littler = Player(25)
 
 plot_dartboard()
 
 i = 0
-score = 1000000
-while i < 1000:
+score = 501
+while i < 3:
     if score <= 170 and score!= 169 and score != 168 and score != 166 and score != 165 and score != 163 and score != 162 and score != 159:
-        checkout = luke_the_fluke_shittler.Checkout(score)
+        checkout = luke_littler.Checkout(score)
         print(checkout)
-        luke_the_fluke_shittler.Aim(checkout[0][0] , checkout[0][1])
-        throw = luke_the_fluke_shittler.Throw()
+        luke_littler.Aim(checkout[0][0], checkout[0][1])
+        throw = luke_littler.Throw()
         polar = ConvertToPolar(throw[0], throw[1])
         dartScore = CalculateValue(polar[0], polar[1])
         plt.scatter(polar[1], polar[0], s=3, c='aqua', marker='o',zorder=2)
 
     else:
-        luke_the_fluke_shittler.Aim(20 , "Treble")
+        luke_littler.Aim(20, "Treble")
         print("Aiming for 20 Treble")
-        throw = luke_the_fluke_shittler.Throw()
+        throw = luke_littler.Throw()
         polar = ConvertToPolar(throw[0], throw[1])
         dartScore = CalculateValue(polar[0], polar[1])
         plt.scatter(polar[1], polar[0], s=3, c='aqua', marker='o',zorder=2)
